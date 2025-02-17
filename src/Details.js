0cfc222f6459e7bd70cf5f40d54433f482fbdc93
manoz-max <manozbunny@gmail.com>
2025-02-17 20:43:30 +0530

    first commit

---
+++ src/Details.js
@@ -0,0 +1,94 @@
+// Enter all your detials in this file
+// Logo images
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import js from "./assets/techstack/js.png";
+import python from "./assets/techstack/python.png";
+import c from "./assets/techstack/c.png";
+import java from "./assets/techstack/java.png";
+import googlecloud from "./assets/techstack/googlecloud.png";
+import flutter from "./assets/techstack/flutter.png";
+import jupyter from "./assets/techstack/jupyter.png";
+import github from "./assets/techstack/github.png";
+import vscode from "./assets/techstack/vscode.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+// import projectImage2 from "./assets/projects/project2.jpg";
+// import projectImage3 from "./assets/projects/project3.jpg";
+// import projectImage4 from "./assets/projects/project4.jpg";
+// import projectImage5 from "./assets/projects/project5.jpg";
+// import projectImage6 from "./assets/projects/project6.jpg";
+
+
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Malleswari ",
+  tagline: "Tech Enthusiast",
+  img: profile,
+  about: `Motivated B.Tech student in third year with a passion for technology and problem-solving. programming languages like Java and Python. Eager to learn and contribute to innovative projects As an ambitious computer science undergraduate with a passion for development, I am eager to contribute my technical skills and creativity to real-world projects. knowing programming languages such as Python, Java, and C, I have knowledge coursework in data structures, algorithms, and web development. My strong problem-solving abilities and collaborative mindset make me an asset to any development team. I am excited to apply my knowledge and learn from experienced professionals in the industry`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://linkedin.com/in/malliswari-boggavarapu",
+  github: "https://github.com/Malliswari18",
+};
+
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor of Engineering-BE",
+   college: "Geethanjali Institute of Science and Technology",
+    Location: "Nellore-524137",
+    Duration: "Jan 2022 - Present",
+  },
+  {
+    Position: "Intermediate",
+    College: `Jagan's Junior college`,
+    Location: "Nellore",
+    Duration: "Aug 2020 - Mar 2022",
+  },
+
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  python: python,
+  c: c,
+  java: java,
+  googlecloud: googlecloud,
+  flutter: flutter,
+  github: github ,
+  jupyter:jupyter,
+  vscode:vscode,
+ 
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Community service project",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    githubLink: "https://github.com",
+  },
+ 
+
+ 
+ 
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "malliswarimahendra@gmail.com",
+};
